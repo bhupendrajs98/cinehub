016da57001ee8cebf5c452a4e3a3c31452b3f839
bhupendrajs98 <bhupendrajsx98@gmail.com>
2026-01-16 22:25:11 +0530

    Fix CORS for frontend with credentials

--- server.js
+++ server.js
@@ -21,8 +21,12 @@ connectDB();
 
 const app = express();
 
+
 // ---------- Global Middleware ----------
-app.use(cors());
+app.use(cors({
+  origin: "http://localhost:5173", // frontend dev URL
+  credentials: true,
+}));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
