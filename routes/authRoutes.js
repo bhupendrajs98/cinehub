15c9bc363ccb6b59c87cdb16f1e9f119cb953622
bhupendrajs98 <bhupendrajsx98@gmail.com>
2026-01-18 02:59:29 +0530

    fix the bug

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,6 +1,6 @@
 import express from "express";
 import { register, login } from "../controllers/authController.js";
-import { protect, adminOnly } from "../middleware/authMiddleware.js";
+import { protect, adminOnly } from "../middlewares/authMiddleware.js";
 import { getAllUsers } from "../controllers/userController.js";
 
 const router = express.Router();
