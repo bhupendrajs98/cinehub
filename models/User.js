8352a8114e9ded14e459578afca4db0c8d5b6ad6
bhupendrajs98 <bhupendrajsx98@gmail.com>
2026-01-18 19:23:31 +0530

    fix a model in user.js

--- models/User.js
+++ models/User.js
@@ -48,20 +48,12 @@ const userSchema = new mongoose.Schema(
 /**
  * 🔐 Hash password before save
  */
-userSchema.pre("save", async function (next) {
-  try {
-    // Only hash if password is modified or new
-    if (!this.isModified("password")) {
-      return next();
-    }
+userSchema.pre("save", async function () {
+  // Only hash if password is modified or new
+  if (!this.isModified("password")) return;
 
-    const salt = await bcrypt.genSalt(10);
-    this.password = await bcrypt.hash(this.password, salt);
-
-    next();
-  } catch (error) {
-    next(error);
-  }
+  const salt = await bcrypt.genSalt(10);
+  this.password = await bcrypt.hash(this.password, salt);
 });
 
 /**
